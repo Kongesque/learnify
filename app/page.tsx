ddf4a0ea5b914aaafe3ff033fe5636557eeb25e9
Kongesque <Kongesque@gmail.com>
2025-09-09 22:38:28 +0800

    implement clerk with supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,49 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'> Welcome! </h1>
-      <Button> 
-        Click me
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'> Popular Companions </h1>
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="789"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language Arts"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionList 
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
